477b87995c6a06df7987f6e0279b0e5a9d1fb8af
willemodendaal <willem@newmedialabs.co.za>
2018-05-18 17:56:18 +0200

    WIP trying to get docker file and webpack and dotnet playing nicely together.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Scripts loaded ok!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends Component {
+    render() {
+      return (
+        <div>
+          Hello there. This is a react component.
+        </div>
+      );
+    }
+  }
+
+  ReactDOM.render(     
+    <Hello />,
+    document.getElementById('container')
+  );
+
+console.log('Scripts loaded ok!23');
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,21 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle2.js',
     path: path.resolve(__dirname, 'wwwroot/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env','react']
+          }
+        }
+      }
+    ]
   }
 };
