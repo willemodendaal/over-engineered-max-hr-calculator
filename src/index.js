1799bc8f48eb96b8b2d56f6c2d36ce1fd63c47fe
willemodendaal <willem@newmedialabs.co.za>
2018-07-04 07:33:34 +0200

    Fixed some React script issues.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends Component {
+class Hello extends React.Component {
     render() {
       return (
         <div>
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle2.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'wwwroot/dist')
   },
   module: {
