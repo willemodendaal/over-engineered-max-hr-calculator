5c4a8c9a50a2a7ea3bbe7db32eedeba12c9af7a0
willemodendaal <willem@newmedialabs.co.za>
2018-08-14 06:40:40 +0200

    Split reducers into modules.

---
+++ src/reducers/calculatorReducer.js
@@ -0,0 +1,14 @@
+
+const initialCalculatorState = 
+{
+	age: null, 
+	maxHr: null 
+};
+
+const reduceCalculator = (oldState = initialCalculatorState, action) =>
+{
+	//Not doing any calculator logic yet.
+	return oldState;
+}
+
+export default reduceCalculator;
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,56 +1,6 @@
 import {combineReducers } from 'redux';
-
-const initialAuthState = 
-{
-	loggedIn: false,
-	name: null,
-	id: null,
-	message: null,
-	busyLoggingOut: false
-};
-const reduceAuth = (oldState = initialAuthState, action) =>
-{
-	switch(action.type)
-	{
-		case 'LOGIN_COMPLETE':
-			return Object.assign({}, oldState, {
-				message: null,
-				loggedIn: true,
-				busyLoggingOut: false
-			});
-		case 'LOGOUT_START':
-			return Object.assign({}, oldState, {
-				message: 'Logging out...',
-				loggedIn: true,
-				busyLoggingOut: true
-			});
-		case 'LOGOUT_COMPLETE':
-			return Object.assign({}, oldState, {
-				loggedIn: false,
-				error: null,
-				busyLoggingOut: false,
-				message: null
-			});
-		case 'LOGOUT_FAILED':
-			return Object.assign({}, oldState, {
-				message: action.error,
-				loggedIn: true,
-				busyLoggingOut: false
-			});
-	}
-	return oldState;
-}
-
-const initialCalculatorState = 
-{
-	age: null, 
-	maxHr: null 
-};
-const reduceCalculator = (oldState = initialCalculatorState, action) =>
-{
-	//Not doing any calculator logic yet.
-	return oldState;
-}
+import reduceAuth from './authReducer';
+import reduceCalculator from './calculatorReducer';
 
 export default combineReducers({
 	auth: reduceAuth,
